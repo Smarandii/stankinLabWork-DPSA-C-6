35a1bc9356810613316ba9069f9484b967f2d7e5
Oleja <olegsmarandi@gmail.com>
2020-12-16 15:49:38 +0300

    задание на тройку завершено

--- Source.cpp
+++ Source.cpp
@@ -1,79 +1,3 @@
-// Внешние сортировки  работают с файлами
-//
-// Серия - отсортированная подпоследовательность элементов.
-// Сортировки слиянием объединяют две или более последовательности в одну серию:
-//
-// Двух-фазная сортировка прямым слиянием.  
-// 3 файла: f, g, t
-// из f формируются файлы g, t (исходный файл f скопировать для сохранности)
-// из g, t формируется f 
-// f - 279 58 -1 7 29 45 8
-// g - 279 | -1 | 29 | 8
-// t - 58 | 7 | 45 |
-// фаза слияния:
-// сравниваем числа из t и g, меньшее записываем в f
-// f - 58 279 | -1 7 | 29 45 | 8
-// повторяем цикл сначала, пока в файле f не останется 1 серия.
-//
-// g - 58 279 | 29 45 |
-// t - -1 7 | 8
-// --------------------
-// f - -1 7 58 279 | 8 29 45 |
-// --------------------
-// g - -1 7 58 279 |
-// t - 8 29 45 |
-// --------------------
-// f - -1 7 8 29 45 58 279
-//
-// Однофазная сортировка прямым слиянием:
-// получена слиянием двух фаз двузфазной сортировки
-// 5 файлов: f, f1, f2, g1, g2,
-// 1. читаем по 1 серии из f1 и f2
-// 2. сливаем эти серии
-// 3. присоединяем результирующую серию к одному из файлов g1 и g2 (они организованны в виде серий длинной в 2k) при этом переключаемся поочередно между g1 g2. хвост сливается с последней из созданных серий
-// 4. опустошаем f1 f2
-// 5. выполняем шаги 1-4 меняя файлы f1 на g1 f2 на g2 пока не получим файл из одной серии
-//
-// f - 279 58 -1 7 29 45 8
-// ------------------
-// f1 - 279 | -1 | 29 | 8
-// f2 - 58 | 7 | 45
-// g1 - 58 279 | 29 45
-// g2 - -1 7 | 8
-// ------------------
-// f1 -1 7 58 279 | 
-// f2 - 8 29 45 |
-// g1 - -1 7 8 29 45 58 279
-// g2 - ...
-//
-// при прямом слиянии мы теряем преимущество от изначально отсортированых данных, так как разделяем а потом вновь сливаем их
-//
-// Сортировка естественным слиянием:
-// Сортировка при которой сливаются две самые длинные серии
-// 3 файла: f, g, t
-//
-// 1. из f серии попеременно распределяются в файл g и t
-// 2. из g и t серии сливаются в f
-// процесс заканчивается как только в файле f останется 1 серия
-//
-// f - 279 58 -1 7 29 45 8
-// --------------------
-// g - 279 | -1 7 29 45 
-// t - 58 | 8 
-// f - 58 279 | -1 7 8 29 45
-// --------------------
-// g - 58 279 |
-// t - -1 7 8 29 45 |
-// f - -1 7 8 29 45 58 279 
-//
-// ускорение сортировок прямым слиянием:
-// для ускорения считываем группы из k записей, сортируем их в оперативной памяти, отсортированные записи записать в файлы в виде серии длинны k 
-// временные затраты пропорциональны числу требуемых проходов по файлам, следовательно надо стремиться сократить кол-во обращений к файлам
-//
-// Задание по лабе:
-// знать теорию по 3 методам сортировки
-// на оценку 4 два метода внешней сортировки
-
 #include <iostream>
 #include <fstream>
 #include<stdio.h>
@@ -130,6 +54,41 @@ int print_file(const char* file_name) {
 	return file_length;
 }
 
+int print_file(const char* file_name, int series_length) {
+	FILE* f;
+	char c = '1';
+	int length_f = 0;
+	printf("\n");
+	f = fopen(file_name, "r");
+	check_file(f);
+	int remaining_length_2 = 0;
+	printf(file_name);
+	printf(": ");
+	while ((c = fgetc(f)) != EOF) {
+		while (c != ' ' && c != EOF) {
+			printf("%c", c);
+			c = fgetc(f);
+
+		}
+		length_f++;
+		remaining_length_2++;
+		if (remaining_length_2 == series_length) {
+			printf("|");
+			remaining_length_2 = 0;
+		}
+		printf("%c", c);
+		c = fgetc(f);
+		if (c == EOF && remaining_length_2 != 0)
+			printf("|");
+		printf("%c", c);
+	}
+	fclose(f);
+	return length_f;
+	
+}
+
+
+
 void _2phase_sort(int length_1_file) {
 	FILE* f, * g, * t;
 	char g_file[100], t_file[100], c = '1';
@@ -139,16 +98,12 @@ void _2phase_sort(int length_1_file) {
 	int remaining_length, remaining_length_2;
 	while (series_length < length_1_file) {
 		
-		printf("\niteration\n");
 		f = fopen("1.txt", "r");
 		check_file(f);
-
 		g = fopen("2.txt", "w");
 		check_file(g);
-
 		t = fopen("3.txt", "w");
 		check_file(t);
-
 
 		// фаза разделения
 		remaining_length = length_1_file;
@@ -178,60 +133,8 @@ void _2phase_sort(int length_1_file) {
 		fclose(t);
 		fclose(f);
 
-		// вывод файла g.txt
-		printf("\n");
-		g = fopen("2.txt", "r");
-		check_file(g);
-		remaining_length_2 = 0;
-		printf("g: ");
-		while ((c = fgetc(g)) != EOF) {
-			while (c != ' ' && c != EOF) {
-				printf("%c", c);
-				c = fgetc(g);
-
-			}
-			length_2_file++;
-			remaining_length_2++;
-			if (remaining_length_2 == series_length) {
-				printf("|");
-				remaining_length_2 = 0;
-			}
-			printf("%c", c);
-			c = fgetc(g);
-			if (c == EOF && remaining_length_2 != 0)
-				printf("|");
-			printf("%c", c);
-		}
-		fclose(g);
-
-		// вывод файла t.txt
-		printf("\n");
-		t = fopen("3.txt", "r");
-		check_file(t);
-		printf("t: ");
-		remaining_length_2 = 0;
-		while ((c = fgetc(t)) != EOF) {
-			while (c != ' ' && c != EOF) {
-				printf("%c", c);
-				c = fgetc(t);
-
-			}
-			length_3_file++;
-			remaining_length_2++;
-			if (remaining_length_2 == series_length) {
-				printf("|");
-				remaining_length_2 = 0;
-			}
-			printf("%c", c);
-			c = fgetc(t);
-			if (c == EOF && remaining_length_2 != 0)
-				printf("|");
-			printf("%c", c);
-
-		}
-		printf("\n");
-		fclose(t);
-
+		length_2_file = print_file("2.txt", series_length);
+		length_3_file = print_file("3.txt", series_length);
 		f = fopen("1.txt", "w");
 		check_file(f);
 		g = fopen("2.txt", "r");
@@ -239,14 +142,15 @@ void _2phase_sort(int length_1_file) {
 		t = fopen("3.txt", "r");
 		check_file(t);
 
+
 		remaining_length = length_1_file;
 		fscanf(g, "%s", g_file);
 		fscanf(t, "%s", t_file);
 		x2 = atoi(g_file);
 		x3 = atoi(t_file);
 
-		// фаза слияния
-		printf("f: ");
+
+		printf("\nf: ");
 		while (remaining_length > 0) {
 			series_length_1_file = series_length;
 			series_length_2_file = series_length;
